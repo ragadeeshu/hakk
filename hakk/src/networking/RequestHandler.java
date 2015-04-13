8ab8373b14c7fdd6701732a36f26fe7ae29e45c1
Ragnar Mellbin <ragnar_mellbin@hotmail.com>
2015-04-13 16:38:57 +0200

    Server now sends objects

--- hakk/src/networking/RequestHandler.java
+++ hakk/src/networking/RequestHandler.java
@@ -2,40 +2,49 @@ package networking;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class RequestHandler implements Runnable {
 
 	private Socket socket;
-	
-	public RequestHandler(Socket socket){
+	private Server server;
+	private ObjectInputStream inputStream = null;
+	private ObjectOutputStream outputStream = null;
+
+	public RequestHandler(Socket socket) {
 		this.socket = socket;
 	}
 
 	@Override
 	public void run() {
-		
-		try{
-			System.out.println("New connection from "+socket.getInetAddress().getHostName()+":"+socket.getPort()+". Local port: "+socket.getLocalPort());
-			InputStream inputStream = socket.getInputStream();
-			OutputStream outputStream = socket.getOutputStream();
-			int ch = inputStream.read();
-			while(ch!=-1){
-				System.out.println(ch);
-				outputStream.write(Character.toUpperCase((char) ch));
-				ch = inputStream.read();
+
+		System.out.println("New connection from "
+				+ socket.getInetAddress().getHostName() + ":"
+				+ socket.getPort() + ". Local port: " + socket.getLocalPort());
+
+		try {
+			inputStream = new ObjectInputStream(socket.getInputStream());
+			outputStream = new ObjectOutputStream(socket.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		new Timer().schedule(new TimerTask() {
+
+			public void run() {
+				try {
+					server.updateCharacterState(socket.getInetAddress()
+							.toString(), inputStream.readObject());
+					outputStream.writeObject(server.getStates());
+				} catch (ClassNotFoundException | IOException e) {
+					e.printStackTrace();
+				}
 			}
-		} catch(IOException e){
-			System.out.println(e);
-		}
-		
-		try{
-			socket.close();
-		} catch(IOException e){
-			System.out.println(e);
-		}
-		
+		}, 0, 17);
+
 	}
-	
+
 }
--- hakk/src/networking/Server.java
+++ hakk/src/networking/Server.java
@@ -1,43 +1,65 @@
 package networking;
 
-import java.io.*;
-import java.net.*;
-import java.util.concurrent.*;
+import game.CharacterState;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
-	
-	public Server(){
-		
+	private HashMap<String, CharacterState> states;
+
+	public Server() {
+
 		ServerSocket serverSocket = null;
 		int portNbr = 4444;
-		
-		try{
+
+		try {
 			serverSocket = new ServerSocket(portNbr);
-		} catch(IOException e){
+		} catch (IOException e) {
 			System.out.println(e);
 			System.exit(1);
 		}
-		
+
 		System.out.println("Server setup successfully.");
 		System.out.println("Waiting for clients to connect...");
-		
+
 		ExecutorService pool = Executors.newFixedThreadPool(4);
-		
-		while(true){
+
+		while (true) {
 			Socket socket = null;
-			try{
+			try {
 				socket = serverSocket.accept();
 				pool.submit(new RequestHandler(socket));
-			} catch(IOException e){
+			} catch (IOException e) {
 				System.out.println(e);
+				try {
+					socket.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
 				System.exit(1);
 			}
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		Server server = new Server();
+	}
+
+	public synchronized void updateCharacterState(String inetAddress,
+			Object readObject) {
+		states.put(inetAddress, (CharacterState) readObject);
+
+	}
+
+	public synchronized Object getStates() {
+		return states;
 	}
 
 }
