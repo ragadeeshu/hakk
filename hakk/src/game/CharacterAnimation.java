2c10263b687fc2b7de9801355d9f8048f8da2f6f
Fishmoose <felix.sw3@gmail.com>
2015-04-23 10:04:31 +0200

    missing file

---
+++ hakk/src/game/CharacterAnimation.java
@@ -0,0 +1,39 @@
+package game;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+public class CharacterAnimation {
+	private static final int NUM_RUNNING = 11;
+	private ArrayList<Image> running;
+	private int current;
+
+	public CharacterAnimation(String baseName) {
+		running = new ArrayList<Image>();
+		BufferedImage img = null;
+		for (int i = 0; i < NUM_RUNNING; i++) {
+			System.out.println("sprites"+File.pathSeparator+baseName+"runni__"+String.format("%03d", i)+".png");
+			try {
+				img = ImageIO.read(new File("sprites/"+baseName+"runni__"+String.format("%03d", i)+".png"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			running.add(img);
+		}
+		current = 0;
+	}
+
+	public void run() {
+		current = (current + 1) % NUM_RUNNING;
+	}
+
+	public Image getImage() {
+		return running.get(current);
+	}
+
+}
