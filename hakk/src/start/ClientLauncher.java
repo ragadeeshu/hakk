6cccca38fdbcad151d6f0b58d95625a8a509f4d9
Fishmoose <felix.sw3@gmail.com>
2015-05-20 15:05:00 +0200

    moved stuff around because: improvements

--- hakk/src/start/ClientLauncher.java
+++ hakk/src/start/ClientLauncher.java
@@ -18,24 +18,24 @@ public class ClientLauncher {
 
 	public static void main(String[] args) throws InterruptedException {
 		Scanner scan = new Scanner(System.in);
-
+		
+		String serverAddress = "127.0.0.1";
+		// serverAddress = "falk-2";
 		// System.out.println("Enter server address: ");
 		// String serverAddress = scan.nextLine();
-		String serverAddress = "127.0.0.1";
-//		serverAddress = "falk-2";
 
+		String playerName = "Player "
+				+ new SimpleDateFormat("mm:ss").format(new Date());
 		// System.out.println("Enter playername (alphanumerical): ");
 		// while (!scan.hasNext("[A-Za-z0-9\\s]+")) {
 		// System.out.println("THAT'S NOT VERY ALPHANUMERICAL. Try again: ");
 		// scan.nextLine();
 		// }
 		// String playerName = scan.nextLine();
-		String playerName = "Player "
-				+ new SimpleDateFormat("mm:ss").format(new Date());
 
 		scan.close();
 		Client client = null;
-		try{
+		try {
 			client = new Client(serverAddress, playerName);
 		} catch (NullPointerException e) {
 			System.out.println("Unable to connect to server.");
@@ -57,7 +57,7 @@ public class ClientLauncher {
 		new UpdateThread(client, stage, playerName).start();
 		System.out.println("Music by Gichco from http://opengameart.org/");
 
-//		new MusicPlayer("bgm/cave-loop.mp3", true).start();
+		// new MusicPlayer("bgm/cave-loop.mp3", true).start();
 
 	}
 }
